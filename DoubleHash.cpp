b44c324e0bd64a0166cc48719ba76f632cfaa91d
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-07-23 20:23:47 +0530

    hashmaps

---
+++ DoubleHash.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string hashTable[21];
+int hashTableSize = 21;
+
+int hashFun(){
+    // Make a function
+}
+
+// Another hashing function
+int hashFun2(){
+    // Make a function
+}
+
+void insert(string s){
+    
+    int index = hashFun(s); // get hash val
+
+    // search for an unused slot and if the index
+    // exceeds the hashTableSize then roll back
+
+    while(hashTable[index] != ""){
+        index = (index + hashFun2) % hashTableSize;
+    }
+    hashTable[index] = s;
+}
+
+void search(string s){
+
+    int index = hashFun(s);
+
+    // Search for the unused slot and if the 
+    // index will exceed the hashTableSize will 
+    // roll back
+
+    while(hashTable[index] != s and hashTable[index] != ""){
+        index = (index + hashFun2()) % hashTableSize;
+    }
+    //check if the table has the element
+    if(hashTable[index] == s){
+        cout << s << " is found " << endl;
+    } else 
+        cout << s << " is not found " << endl;
+        
+}
