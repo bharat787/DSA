592804747bcc1aae23f35fb0070833245ee439c8
bharat787 <bgbharat787@gmail.com>
2020-07-27 20:29:35 +0530

    linklist node rem

---
+++ LC/removeNthNodeFromEnd.py
@@ -0,0 +1,16 @@
+def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+        
+        for i in range(n+1):
+            first = first.next
+            
+        while( first != None):
+            first = first.next
+            second = second.next
+            
+        second.next = second.next.next
+        
+        return dummy.next
