01e4176d09e6364940f082a7340d0040f6d55e23
bharat787 <bgbharat787@gmail.com>
2020-08-03 22:54:35 +0530

    super set backtracking

---
+++ LC/superSet.py
@@ -0,0 +1,11 @@
+def dfs(self, index, res, path, nums):
+        res.append(list(path))        
+        for i in range(index, len(nums)):
+            path.append(nums[i])
+            self.dfs(i+1, res, path, nums)
+            path.pop()
+
+    def subsets(self, nums):
+        res, path = [], []
+        self.dfs(0, res, path, nums)
+        return res
