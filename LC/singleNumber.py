091ccfdb6d3f3742f9aafc8497ab0167acf67bf7
bharat787 <bgbharat787@gmail.com>
2020-08-14 16:10:45 +0530

    using hashmap to store occurance

---
+++ LC/singleNumber.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        hashtable = defaultdict(int)
+        for i in nums:
+            hashtable[i] += 1
+        
+        for i in hashtable:
+            if hashtable[i] == 1:
+                return i
