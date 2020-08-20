d7b671dcb7cc5ecf34d05bc800c8f4d4ce97c29e
bharat787 <bgbharat787@gmail.com>
2020-08-20 21:55:43 +0530

    -

--- LC/powerOf2.py
+++ LC/powerOf2.py
@@ -2,10 +2,8 @@ import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
-        if n==1:
+        if n < 0:
+            return False
+        if bin(n).count('1') == 1:
             return True
-        while(n):
-            if n==2:
-                return True
-            n/=2
         return False
