3879c52062b9142205151e4b2e2336a8654d164a
bharat787 <bgbharat787@gmail.com>
2020-08-24 20:26:31 +0530

    LC240

---
+++ LC/search2D.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0:
+            return False
+        
+        for i in matrix:
+            if target in i:
+                return True
+            
+        return False
+            
