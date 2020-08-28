ad0be603cf128b59489bcee1170d9824bedd76ff
bharat787 <bgbharat787@gmail.com>
2020-08-28 16:23:38 +0530

    -

---
+++ LC/citationsII.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        if not citations:
+            return 0
+        #citations.sort()
+        citations.reverse()
+        for idx, cit in enumerate(citations):
+            if cit <= idx:
+                return idx
+        return len(citations)  
